b55a829a02be81643f1fb0df2539ead962062b12
Schandroid243 <schadrackngunza@gmail.com>
2023-06-13 14:25:16 +0100

    edited contact

--- pages/contact/add/_id.vue
+++ pages/contact/add/_id.vue
@@ -3,7 +3,7 @@
       <b-row class="align-items-center justify-content-center">
         <div class=" col-md-9 col-xl-9 col-lg-9 text-dark">
           <b-container class="mt-4 mb-4">
-            <h3 class="text-white">Créer un nouveau contact pour le client :</h3>
+            <h3 class="text-white">Créer un contact pour le client :</h3>
           </b-container>
           <b-container fluid v-if="loader" class="mt-4 mb-4">
             <b-row>
